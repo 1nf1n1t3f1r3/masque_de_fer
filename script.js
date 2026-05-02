3231075afd22dd23705b817d6e0b240f13133aad
1nf1n1t3f1r3 <1nf1n1t3f1r3@gmail.com>
2026-05-02 14:29:14 +0200

    Added Mobile Hamburger

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ fetch("partials/footer.html")
 
     // footer-related JS (if any later)
   });
+
+//   Hamburger
+// Load header
+fetch("partials/header.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+
+    // ✅ NOW the elements exist → attach events
+
+    const toggle = document.querySelector(".menu_toggle");
+    const menu = document.querySelector(".nav_menu");
+
+    if (toggle && menu) {
+      toggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+      });
+    }
+  });
